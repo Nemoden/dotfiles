1019b5cdb88497ef4d49ad497984e71be5d3da1a
Kirill Kovalchuk <kkovalchuk@deputy.com>
2023-09-14 12:48:15 +1000

    @ function, added fzf completions

--- .config/fish/functions/@.fish
+++ .config/fish/functions/@.fish
@@ -1,3 +1,11 @@
 function @ --description "Exec -it into docker container"
-    docker exec -it $argv
+    if test (count $argv) -eq 0
+        set container (docker ps --format '{{ .Image }}\t{{ .Names }}' | fzf | awk '{print $2}')
+        set shells bash sh
+        set shell (string collect $shells | fzf)
+        echo "docker exec -it $container $shell"
+        docker exec -it $container $shell
+    else
+        docker exec -it $argv
+    end
 end

7cd32a0a709a0d10f7411e0bb080751270316199
Kirill Kovalchuk <kkovalchuk@deputy.com>
2025-04-23 14:38:13 +1000

    make gli preview scrollable

--- .config/fish/functions/gli.fish
+++ .config/fish/functions/gli.fish
@@ -1,3 +1,3 @@
 function gli --wraps='git log' --description 'git log preview (via fzf)'
-    git log --format="%h : %as : %an" $argv | fzf --preview='git show --color=always {+1}' --preview-window=right:70%
+    git log --format="%h : %as : %an" $argv | fzf --preview='git show --color=always {+1}' --preview-window=right:70% --bind 'ctrl-f:preview-half-page-down,ctrl-b:preview-half-page-up'
 end

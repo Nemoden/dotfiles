07a790a08cee2bc704bc7d8b0cc08cf4b0ea219c
Kirill Kovalchuk <kkovalchuk@atlassian.com>
2025-09-09 21:55:44 +1000

    use brew prefix for nvm

--- .config/fish/functions/nvm.fish
+++ .config/fish/functions/nvm.fish
@@ -1,6 +1,7 @@
 function nvm
-    if test -f /usr/local/opt/nvm/nvm.sh
-        bax source /usr/local/opt/nvm/nvm.sh --no-use ';' nvm $argv
+    set -l brew_prefix (brew --prefix)
+    if test -f $brew_prefix/opt/nvm/nvm.sh
+        bax source $brew_prefix/opt/nvm/nvm.sh --no-use ';' nvm $argv
     end
     if test -f ~/.nvm/nvm.sh
         bax source ~/.nvm/nvm.sh --no-use ';' nvm $argv
